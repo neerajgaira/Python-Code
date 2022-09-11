53325148067dc00e6a9113590f102b8e7dc3d517
neerajgaira <113292807+neerajgaira@users.noreply.github.com>
2022-09-11 18:58:08 +0530

    Add files via upload

---
+++ nrj02.py
@@ -0,0 +1,5 @@
+# program to obtain l and b of rectangle and calculate its perimeter and area
+length = int(input("Enter the length of a rectangle : "))
+breadth = int(input("Enter the breadth of a rectangle : "))
+print("The perimeter of a rectangle is : ",2*(length+breadth))
+print("The area of a rectangle is : ",length*breadth)
